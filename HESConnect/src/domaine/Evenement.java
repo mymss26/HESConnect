28ad5b64673905b62c524c6f2f72fd230ac5c0c6
mymss26 <myriam.gazaoui@hes-so.ch>
2022-12-08 14:37:35 +0100

    Insertion et création de la classe Evenement

--- HESConnect/src/domaine/Evenement.java
+++ HESConnect/src/domaine/Evenement.java
@@ -1,4 +1,26 @@
 package domaine;
 
+import java.util.Arrays;
+
 public class Evenement {
+    String nomEvenement;
+    String[] thematique;
+
+    public Evenement(String nomEvenement, String[] thematique) {
+        this.nomEvenement = nomEvenement;
+        this.thematique = thematique;
+    }
+
+    public String getNomEvenement() {
+        return nomEvenement;
+    }
+
+    public String[] getThematique() {
+        return thematique;
+    }
+
+    @Override
+    public String toString() {
+        return "Evenement:" + nomEvenement + ", thematique :" + Arrays.toString(thematique) ;
+    }
 }
